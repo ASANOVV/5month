996109588d22c5795add982e9779395f924a6e54
ASANOVV <mirbek020295@gmail.com>
2022-12-16 16:46:06 +0300

    hw1

---
+++ movie_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+    director = models.ForeignKey(Director, related_name='movies', on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    text = models.TextField()
+    movie = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
